21abb7ef271452c28a28d0cfb316fd87ac2b2095
KilluaProject <134472737+KilluaProject@users.noreply.github.com>
2023-05-31 13:27:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 var numberOfDrumButton = document.querySelectorAll(".drum").length;
 
-for(var i = 0; i < numberOfDrumButton; + i++){
+for(var i = 0; i < numberOfDrumButton; + i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         
         var buttonInnerHTML = this.innerHTML;
 
         makeSounds(buttonInnerHTML);
+
+        activeButton(buttonInnerHTML);
 
 
 
@@ -17,6 +19,8 @@ for(var i = 0; i < numberOfDrumButton; + i++){
 
 document.addEventListener("keypress", function(e){
     makeSounds(e.key);
+
+    activeButton(e.key);
 });
 
 
@@ -25,44 +29,39 @@ function makeSounds(key){
 
     switch(key){
         case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3")
+        var tom1 = new Audio("sounds/pikachu.mp3")
         tom1.play();
         break;
 
         case "s":
-        var tom2 = new Audio("sounds/tom-2.mp3")
+        var tom2 = new Audio("sounds/charmender.mp3")
         tom2.play();
         break;
 
         case "a":
-        var tom3 = new Audio("sounds/tom-3.mp3")
+        var tom3 = new Audio("sounds/squirtle.mp3")
         tom3.play();
         break;
 
         case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3")
+        var tom4 = new Audio("sounds/bulbasaur8.mp3")
         tom4.play();
-        break;
-
-        case "j":
-        var kickbass = new Audio("sounds/kick-bass.mp3")
-        kickbass.play();
-        break;
-
-        case "k":
-        var crash = new Audio("sounds/crash.mp3")
-        crash.play();
-        break;
-
-        case "l":
-        var snare = new Audio("sounds/snare.mp3")
-        snare.play();
         break;
 
 
         default: console.log(buttonInnerHTML);
     }
 
+}
+
+function activeButton(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+    activeButton.classList.remove("pressed");
+    },100);
 }
 
 // var audio = new Audio('sounds/tom-1.mp3');
